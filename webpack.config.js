7978555e6d06b7f631c9fb8b1ade744ab065444d
Michal <michalgrzegorczyk04@gmail.com>
2021-09-08 15:22:18 +0200

    Add babel to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
-const path = require('path');
+let mode = "development";
+
+if (process.env.NODE_ENV === "production")
+    mode = "production";
 
 module.exports = {
-    mode: 'development',
+    mode: mode,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    devtool: "source-map",
     devServer: {
-        static: "./dist"
+        static: "./dist",
     },
 };
