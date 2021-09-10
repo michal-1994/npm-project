c2c91ffa0bfb229c45af7be30014e39f7cd73b7f
Michal <michalgrzegorczyk04@gmail.com>
2021-09-10 11:44:13 +0200

    Add footer section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import "./styles/index.scss";
 import $ from "jquery";
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import "./styles/index.scss";
 
 const hello = (name) => {
     console.log(`Hello ${name}.`);
